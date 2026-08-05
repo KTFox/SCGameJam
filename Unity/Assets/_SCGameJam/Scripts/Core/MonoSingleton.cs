5fd6ed0f85b846da7f54b964e3f3d6bfcba85484
SCVN-Kai <Kai@supercent.vn>
2026-08-05 17:55:39 +0700

    Implement MonoSingleton

--- Unity/Assets/_SCGameJam/Scripts/Core/MonoSingleton.cs
+++ Unity/Assets/_SCGameJam/Scripts/Core/MonoSingleton.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
 
-namespace SCJam
+namespace SCJam.Core
 {
     public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
     {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+            }
+        }
     }
 }
