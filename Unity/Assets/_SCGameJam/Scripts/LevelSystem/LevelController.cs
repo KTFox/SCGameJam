44e372b806d96de44d00e16324c06795886ee367
SCVN-Kai <Kai@supercent.vn>
2026-08-20 20:37:29 +0700

    Add sfx

--- Unity/Assets/_SCGameJam/Scripts/LevelSystem/LevelController.cs
+++ Unity/Assets/_SCGameJam/Scripts/LevelSystem/LevelController.cs
@@ -24,6 +24,8 @@ namespace SCJam.LevelSystem
         [SerializeField] private PassengerQueueView _passengerQueueView;
         [SerializeField] private Transform _vehicleSpawnRoot;
         [SerializeField] private Transform _passengerSpawnRoot;
+        [SerializeField] private SoundSO _winSound;
+        [SerializeField] private SoundSO _loseSound;
 
 
         // ===== Private Fields ===== //
@@ -327,6 +329,8 @@ namespace SCJam.LevelSystem
             _levelState = LevelState.Won;
             Debug.Log("Level completed");
 
+            AudioManager.Instance?.StopMusic();
+            AudioManager.Instance?.PlaySound(_winSound);
             OnLevelCompleted?.Invoke();
             ShowNextLevelPopup();
         }
@@ -360,6 +364,8 @@ namespace SCJam.LevelSystem
             _levelState = LevelState.Lost;
             Debug.Log("Level failed");
 
+            AudioManager.Instance?.StopMusic();
+            AudioManager.Instance?.PlaySound(_loseSound);
             OnLevelFailed?.Invoke();
             ShowLosePopup();
         }
